7245d22ef3982987ffe3422da315178d11af89c6
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-04-06 22:35:11 +0800

    Update list of items

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Tshirt", quantity: 5, packed: true },
 ];
 
 export default function App() {
