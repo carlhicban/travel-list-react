8b602229e601602b357ba51dc29ca55a318b6a9c
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-04-10 22:18:40 +0800

    Add data in intital items

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 2, description: "Tshirt", quantity: 5, packed: true },
+  { id: 2, description: "Pants", quantity: 5, packed: true },
 ];
 
 export default function App() {
